3bba1648d0769b6cdb60c59746c44a76bd49c6f1
Arunkumarrm <marun7m@gmail.com>
2017-08-01 13:33:19 +0530

    1-8-2017 task is to fetch an data from the table and to update it.

--- Udemy testing/Udemy/src/basics/Marvelupdate.java
+++ Udemy testing/Udemy/src/basics/Marvelupdate.java
@@ -1,21 +1,43 @@
 package basics;
 
 
+
+
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class Marvelupdate {
 	
 	public static void Updatedepartement()
 	{
-		System.setProperty("webdriver.chrome.driver","E:\\\\chromedriver.exe" );
+		System.setProperty("webdriver.chrome.driver","E:\\\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.navigate().to("http://localhost/LMS/Department.aspx");
-		driver.findElement(By.xpath(".//*[@id='tableData']/tbody/tr[77]/td[2]/a")).click();
-		
+		//this part of the code is to scroll down so that webdriver can find the web element.
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		 driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		js.executeScript("scroll(0, 250);");
+		 driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		 driver.findElement(By.xpath("(//a[contains(text(),'Edit')])[80]")).click();
+		    
+		 JavascriptExecutor jse = (JavascriptExecutor)driver;
+		 driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		jse.executeScript("scroll(0,-250);");
+    	driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.findElement(By.id("txt_Add")).clear();
+		driver.findElement(By.id("txt_Add")).sendKeys("MSC Information Technology");
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.findElement(By.id("btnupd")).click();
 		
 	}
     	
-
+	public static void main(String[] args) {
+		Marvelupdate.Updatedepartement();
+	}
 }
