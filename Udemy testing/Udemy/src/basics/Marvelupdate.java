ac5bf4e06dc41431af87e6e09288a9e8bf84292d
Arunkumarrm <marun7m@gmail.com>
2017-08-01 15:47:04 +0530

    updated comments and added functionality.

--- Udemy testing/Udemy/src/basics/Marvelupdate.java
+++ Udemy testing/Udemy/src/basics/Marvelupdate.java
@@ -15,21 +15,33 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Marvelupdate {
 	
 	public static void Updatedepartement()
-	{
+	{   
+		//This line of code is calling the calling the chrome driver.
 		System.setProperty("webdriver.chrome.driver","E:\\\\chromedriver.exe");
+		
+		//This line of code is creating an object for the webdriver.
 		WebDriver driver=new ChromeDriver();
+		
+		//This line of code is to navigate the browser to the to the below mentioned site.
 		driver.navigate().to("http://localhost/LMS/Department.aspx");
-		//this part of the code is to scroll down so that webdriver can find the web element.
+		
+		//This part of the code is to scroll down so that webdriver can find the web element.
 		JavascriptExecutor js = (JavascriptExecutor)driver;
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		js.executeScript("scroll(0, 250);");
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		
+		
+		//This line of code is to find an web element using the Xpath.
 		driver.findElement(By.xpath("(//a[contains(text(),'Edit')])[80]")).click();
-		//this part of the code is to scroll up so that webdriver can find the web element.    
+		
+		
+		//This part of the code is to scroll up so that webdriver can find the web element.    
 		JavascriptExecutor jse = (JavascriptExecutor)driver;
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		jse.executeScript("scroll(0,-250);");
     	driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    	//This line of code is to 
 		driver.findElement(By.id("txt_Add")).clear();
 		driver.findElement(By.id("txt_Add")).sendKeys("MSC Information");
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
