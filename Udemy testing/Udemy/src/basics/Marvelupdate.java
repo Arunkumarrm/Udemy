54892a38f88d8c28590131d509b7d1a02266dd7e
Arunkumarrm <marun7m@gmail.com>
2017-08-01 15:18:05 +0530

    Updating the comments on the code(1-8-2017).

--- Udemy testing/Udemy/src/basics/Marvelupdate.java
+++ Udemy testing/Udemy/src/basics/Marvelupdate.java
@@ -21,19 +21,21 @@ public class Marvelupdate {
 		driver.navigate().to("http://localhost/LMS/Department.aspx");
 		//this part of the code is to scroll down so that webdriver can find the web element.
 		JavascriptExecutor js = (JavascriptExecutor)driver;
-		 driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		js.executeScript("scroll(0, 250);");
-		 driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		 driver.findElement(By.xpath("(//a[contains(text(),'Edit')])[80]")).click();
-		    
-		 JavascriptExecutor jse = (JavascriptExecutor)driver;
-		 driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.findElement(By.xpath("(//a[contains(text(),'Edit')])[80]")).click();
+		//this part of the code is to scroll up so that webdriver can find the web element.    
+		JavascriptExecutor jse = (JavascriptExecutor)driver;
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		jse.executeScript("scroll(0,-250);");
     	driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.findElement(By.id("txt_Add")).clear();
-		driver.findElement(By.id("txt_Add")).sendKeys("MSC Information Technology");
+		driver.findElement(By.id("txt_Add")).sendKeys("MSC Information");
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.findElement(By.id("btnupd")).click();
+		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+		
 		
 	}
     	
