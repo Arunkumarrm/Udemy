d20bd7f1bd92b7fd8c7bb9b201ca5911fbfe4d83
Arunkumarrm <marun7m@gmail.com>
2017-08-08 14:52:25 +0530

    todays 8/8/2017 TestNG exercise was to trigger different sites.

---
+++ Udemy testing/Udemy/src/basics/Different_Website_Triggering.java
@@ -0,0 +1,34 @@
+package basics;
+
+import org.testng.annotations.Test;
+
+public class Different_Website_Triggering {
+
+	@Test (priority=1) 
+	public void test1(){
+		Facebooklogin.loggin();		
+		}
+	@Test (priority=2)
+    public void test2()
+    {
+		Helloworld.Library_Version1_login();
+    }
+	
+	@Test (priority=3)
+	public void test3()
+	{
+		Marvel.addepartement();
+	}
+	
+	@Test (priority=4)
+	public void test4()
+	{
+		Marvel_update.Update_department();
+	}
+	
+	@Test (priority=5)
+	public void test5()
+	{
+		New_Exercise_01.Add_cart();
+	}
+}
